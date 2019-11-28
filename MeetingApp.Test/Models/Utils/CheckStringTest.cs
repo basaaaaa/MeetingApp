58d7d1fe8a27ef452ae895171512101d5a6f05a0
matsumoto_ts <matsumoto_ts@mti.co.jp>
2019-11-28 14:44:21 +0900

    add ApplicationPropertiesTest/CheckStringTest

--- MeetingApp.Test/Models/Utils/CheckStringTest.cs
+++ MeetingApp.Test/Models/Utils/CheckStringTest.cs
@@ -9,9 +9,11 @@ namespace MeetingApp.Test.Models.Utils
         public void Setup()
         {
         }
-
+        /// <summary>
+        /// 正常系
+        /// </summary>
         [Test]
-        public void CheckString_isAlphaNumericPlusAlphaOnly_Success()
+        public void CheckString_isAlphaNumericPlusAlphaOnly_Success_AlphabetAndNumber()
         {
 
             var checkString = new CheckString();
@@ -19,5 +21,72 @@ namespace MeetingApp.Test.Models.Utils
 
             Assert.IsTrue(result);
         }
+        [Test]
+        public void CheckString_isAlphaNumericPlusAlphaOnly_Success_AlphabetOnly()
+        {
+
+            var checkString = new CheckString();
+            var result = checkString.isAlphaNumericPlusAlphaOnly("test");
+
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void CheckString_isAlphaNumericPlusAlphaOnly_Success_NumberOnly()
+        {
+
+            var checkString = new CheckString();
+            var result = checkString.isAlphaNumericPlusAlphaOnly("123456789");
+
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void CheckString_isAlphaNumericPlusAlphaOnly_Success_ContainsUnderBar()
+        {
+
+            var checkString = new CheckString();
+            var result = checkString.isAlphaNumericPlusAlphaOnly("12345_6789");
+
+            Assert.IsTrue(result);
+        }
+        /// <summary>
+        /// 失敗系
+        /// </summary>
+        [Test]
+        public void CheckString_isAlphaNumericPlusAlphaOnly_Failure_UnderBarOnly()
+        {
+
+            var checkString = new CheckString();
+            var result = checkString.isAlphaNumericPlusAlphaOnly("______");
+
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void CheckString_isAlphaNumericPlusAlphaOnly_Failure_ContainsKana()
+        {
+
+            var checkString = new CheckString();
+            var result = checkString.isAlphaNumericPlusAlphaOnly("あかさたなABCD");
+
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void CheckString_isAlphaNumericPlusAlphaOnly_Failure_ContainsFullWidth()
+        {
+
+            var checkString = new CheckString();
+            var result = checkString.isAlphaNumericPlusAlphaOnly("１２８７９２３");
+
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public void CheckString_isAlphaNumericPlusAlphaOnly_Failure_SpaceOnly()
+        {
+
+            var checkString = new CheckString();
+            var result = checkString.isAlphaNumericPlusAlphaOnly("      ");
+
+            Assert.IsFalse(result);
+        }
+
     }
 }
